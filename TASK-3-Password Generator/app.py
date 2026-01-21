769873df294fab63559aa9d79a9bd0dd79e8d44f
Preksha-M <prekshapoojary60@gmail.com>
2026-01-21 12:58:32 +0530

    Create app.py

---
+++ TASK-3-Password Generator/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import random
+import string
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    password = ""
+    if request.method == "POST":
+        length = int(request.form["length"])
+
+        characters = string.ascii_letters + string.digits + "!@#$%^&*"
+        password = "".join(random.choice(characters) for i in range(length))
+
+    return render_template("index.html", password=password)
+
+if __name__ == "__main__":
+    app.run(debug=True)
